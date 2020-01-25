62df63aad92925ae36af531c5eaa1ba3668ca76d
Jonathan Lynch <303310+jonathanboom@users.noreply.github.com>
2020-01-25 11:50:08 -0500

    Switch away form malloc/free

--- Sources/StarTychCore/ImageUtils.swift
+++ Sources/StarTychCore/ImageUtils.swift
@@ -29,22 +29,22 @@ class ImageUtils {
     
     static func averageColorComponents(for image: CGImage) -> [CGFloat] {
         // Make the raw space to draw 1 pixel, 4 bytes
-        let rawData = malloc(4)
+        let rawData = UnsafeMutableRawPointer.allocate(byteCount: 4, alignment: 1)
         
         // Draw the image as a 1x1 pixel into a canvas (in 32-bit, big endian format)
         let canvas = CGContext(data: rawData, width: 1, height: 1, bitsPerComponent: 8, bytesPerRow: 4, space: CGColorSpaceCreateDeviceRGB(), bitmapInfo: alphaPremultipliedLast.rawValue)
         canvas?.draw(image, in: CGRect(x: 0, y: 0, width: 1, height: 1))
         
         // Load the color components as Floats, then divide by maximum color value to build the component array
-        let rawDataChars = rawData?.load(as: [CGFloat].self)
+        let rawDataChars = rawData.load(as: [CGFloat].self)
         let comps: [CGFloat] = [
-            (rawDataChars?[0])! / 0xff,
-            (rawDataChars?[1])! / 0xff,
-            (rawDataChars?[2])! / 0xff,
+            rawDataChars[0] / 0xff,
+            rawDataChars[1] / 0xff,
+            rawDataChars[2] / 0xff,
             1.0 // Alpha channel
         ]
         
-        free(rawData)
+        rawData.deallocate()
         return comps
     }
     
